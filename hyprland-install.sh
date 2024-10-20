de40518287fa655ba0f2368fb42a5465c4af30d1
kaung-minkhant <kaungminkhant.official@gmail.com>
2024-10-20 15:32:00 +0700

    added i3 and hyprland

---
+++ hyprland-install.sh
@@ -0,0 +1,7 @@
+
+
+
+echo "this is assumeing nvidia driver version 550.107.02 and cuda verstion 12.4"
+sudo apt install nvidia-utils-550
+sudo dpkg --add-architecture i386
+sudo apt update
---
+++ i3-uninstall.sh
@@ -0,0 +1,2 @@
+sudo apt remove i3 && sudo apt autoremove
+sudo rm -r /etc/i3
